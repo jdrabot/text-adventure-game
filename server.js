3e365d4f545f9af2f70eab9eea0dc03f966a3c0a
Joshua Maney <joshuaraymaney@gmail.com>
2021-07-10 11:12:41 -0400

    deploying

--- server.js
+++ server.js
@@ -19,12 +19,25 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
 
-mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/text_adventure_game')
-  .then(() => {
-    console.log("Connected to Mongoose")
-  }, (err) => {
-    console.log(`Mongoose connection err:\n${err}`)
-  });
+
+mongoose
+  .connect(
+    process.env.MONGODB_URL || "mongodb://localhost/text_adventure_game",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
+  )
+  .then(
+    () => {
+      console.log("Connected to Mongoose");
+    },
+    (err) => {
+      console.log(`Mongoose connection err:\n${err}`);
+    }
+  );
 
 app.use(
   session({
