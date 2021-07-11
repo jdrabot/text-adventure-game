da12ffc170dd2c9c968518f2d2e140fa4244d7a5
Joshua Maney <joshuaraymaney@gmail.com>
2021-07-11 15:11:11 -0400

    working on story

--- client/src/game/textNodes.js
+++ client/src/game/textNodes.js
@@ -230,6 +230,9 @@ export default [
     ],
     imgSrc: "/images/locations/pixelArmory.png",
   },
+
+  //////// WALLY WARRIOR
+
   {
     id: 9,
     text: "Make sure you scour each room for items to use on this quest. Beware, though, good traveler, not all items are helpful. Some prove to be more trouble than they're worth!",
@@ -291,7 +294,7 @@ export default [
       },
       {
         display: true,
-        text: "Open the door at the end of the hallway",
+        text: "Go to the door at the end of the hallway",
         nextText: 14,
       },
     ],
@@ -440,6 +443,441 @@ export default [
     ],
     imgSrc: "/images/locations/pixelMother.png",
   },
-];
+
 
   // cut to end game credits
+
+
+
+
+  //////// MINDY MAGE
+
+  {
+    id: 9,
+    text: "Make sure you scour each room for items to use on this quest. Beware, though, good traveler, not all items are helpful. Some prove to be more trouble than they're worth!",
+    options: [
+      {
+        display: true,
+        text: "Leave the armory",
+        nextText: 10,
+      },
+      {
+        display: true,
+        text: "Attack the burly-looking gentleman",
+        nextText: 11,
+      },
+    ],
+    // burly gentleman
+    imgSrc: "/images/locations/pixelArmory.png",
+  },
+  {
+    id: 11,
+    text: "The burly-looking gentleman is a lot burlier than you thought he was. He kills you with no trouble",
+    options: [
+      {
+        // empty inventory?
+        display: true,
+        text: "Start Again?",
+        nextText: 1,
+        hitFunction: ["RESTART"],
+      },
+    ],
+    // attack splash
+    imgSrc: "/images/locations/pixelArmory.png",
+  },
+  {
+    id: 10,
+    text: "You crack open the door and step out into a dark, stone-walled hallway.",
+    options: [
+      {
+        display: true,
+        text: "Next",
+        nextText: 103,
+      },
+    ],
+    imgSrc: "/images/locations/pixelHall.png",
+  },
+  {
+    id: 103,
+    text: "It smells musty and you can hear shuffling behind some of these doors. You wonder what fresh hell awaits you...",
+    options: [
+      {
+        display: true,
+        text: "Open the door on the left",
+        nextText: 12,
+      },
+      {
+        display: true,
+        text: "Open the door on the right",
+        nextText: 13,
+      },
+      {
+        display: true,
+        text: "Go to the door at the end of the hallway",
+        nextText: 14,
+      },
+    ],
+    imgSrc: "/images/locations/pixelHall.png",
+  },
+  {
+    id: 12,
+    text: "You open the door and step out into what looks like a library...",
+    options: [
+      {
+        display: true,
+        text: "inspect red candle",
+        nextText: 15,
+      },
+      {
+        display: true,
+        text: "inspect bookshelf",
+        nextText: 16,
+      },
+      {
+        display: true,
+        text: "inspect table",
+        nextText: 17,
+      },
+      {
+        display: true,
+        text: "Exit into Hallway",
+        nextText: 10,
+      },
+    ],
+    imgSrc: "/images/locations/pixelLibrary.png",
+  },
+  {
+    id: 15,
+    text: "this candle looks interesting enough to take",
+    display: true,
+    options: [
+      {
+        text: "take the candle",
+        display: true,
+        nextText: 12,
+        setInventory: {
+          items: { redCandle: true },
+        },
+        hitFunction: ["SET_INVENTORY"],
+      },
+    ],
+    imgSrc: "/images/locations/pixelLibrary.png",
+  },
+  {
+    id: 16,
+    text: "you see one of the books looks out of place. Upon further investigation, this book is hollow! Inside, you find a mana potion and a large bronze key",
+    display: true,
+    options: [
+      {
+        text: "take the items",
+        display: true,
+        nextText: 12,
+        setInventory: {
+          items: { mana2: true, bronzeKey2: true },
+        },
+        hitFunction: ["SET_INVENTORY"],
+      },
+    ],
+    imgSrc: "/images/locations/pixelLibrary.png",
+  },
+  {
+    id: 17,
+    text: "On the table, there is a large black candle. Looking around, you see some drawers. All of them are locked except one.",
+    display: true,
+    options: [
+      {
+        text: "take the black candle",
+        display: true,
+        nextText: 12,
+        setInventory: {
+          items: { blackCandle: true },
+        },
+        hitFunction: ["SET_INVENTORY"],
+      },
+      {
+        text: "open the drawer",
+        display: true,
+        nextText: 18,
+      },
+    ],
+    imgSrc: "/images/locations/pixelLibrary.png",
+  },
+  {
+    id: 18,
+    text: "In the drawer, you find another piece of the map that looks like it lines up perfectly with the piece you already have.",
+    display: true,
+    options: [
+      {
+        text: "take the map piece",
+        display: true,
+        nextText: 12,
+        setInventory: {
+          items: { map2: true },
+        },
+        hitFunction: ["SET_INVENTORY"],
+      },
+    ],
+    imgSrc: "/images/locations/pixelLibrary.png",
+  },
+  {
+    // cathedral
+    id: 13,
+    text: "Cathedral... dark priest",
+    options: [
+      {
+        display: true,
+        text: "Place candles in respective holders and light",
+        setInventory: {
+          items: { redCandle: false, blackCandle: false },
+        },
+        hitFunction: ["SET_INVENTORY"],
+
+        nextText: 19,
+      },
+    ],
+    imgSrc: "/images/locations/pixelCath.png",
+  },
+  {
+    id: 19,
+    text: "The dark ritual has begun...",
+    options: [
+      {
+        display: true,
+        text: "Exit into hallway",
+        nextText: 10,
+      },
+    ],
+    imgSrc: "/images/locations/pixelCath.png",
+  },
+  {
+    // must have keys in inventoy to open door... required inventory
+    id: 14,
+    text: "Open the door with keys and you see a large portrait of a woman. The painting seems to be moving... Intruder... handmaids",
+    options: [
+      {
+        display: true,
+        text: "Begin battle",
+        // encounterCode
+      },
+    ],
+    imgSrc: "/images/locations/pixelMother.png",
+  },
+
+
+
+
+
+  //////// ROGER ROGUE
+
+  {
+    id: 9,
+    text: "Make sure you scour each room for items to use on this quest. Beware, though, good traveler, not all items are helpful. Some prove to be more trouble than they're worth!",
+    options: [
+      {
+        display: true,
+        text: "Leave the armory",
+        nextText: 10,
+      },
+      {
+        display: true,
+        text: "Attack the burly-looking gentleman",
+        nextText: 11,
+      },
+    ],
+    // burly gentleman
+    imgSrc: "/images/locations/pixelArmory.png",
+  },
+  {
+    id: 11,
+    text: "The burly-looking gentleman is a lot burlier than you thought he was. He kills you with no trouble",
+    options: [
+      {
+        // empty inventory?
+        display: true,
+        text: "Start Again?",
+        nextText: 1,
+        hitFunction: ["RESTART"],
+      },
+    ],
+    // attack splash
+    imgSrc: "/images/locations/pixelArmory.png",
+  },
+  {
+    id: 10,
+    text: "You crack open the door and step out into a dark, stone-walled hallway.",
+    options: [
+      {
+        display: true,
+        text: "Next",
+        nextText: 103,
+      },
+    ],
+    imgSrc: "/images/locations/pixelHall.png",
+  },
+  {
+    id: 103,
+    text: "It smells musty and you can hear shuffling behind some of these doors. You wonder what fresh hell awaits you...",
+    options: [
+      {
+        display: true,
+        text: "Open the door on the left",
+        nextText: 12,
+      },
+      {
+        display: true,
+        text: "Open the door on the right",
+        nextText: 13,
+      },
+      {
+        display: true,
+        text: "Go to the door at the end of the hallway",
+        nextText: 14,
+      },
+    ],
+    imgSrc: "/images/locations/pixelHall.png",
+  },
+  {
+    id: 12,
+    text: "You open the door and step out into what looks like a library...",
+    options: [
+      {
+        display: true,
+        text: "inspect red candle",
+        nextText: 15,
+      },
+      {
+        display: true,
+        text: "inspect bookshelf",
+        nextText: 16,
+      },
+      {
+        display: true,
+        text: "inspect table",
+        nextText: 17,
+      },
+      {
+        display: true,
+        text: "Exit into Hallway",
+        nextText: 10,
+      },
+    ],
+    imgSrc: "/images/locations/pixelLibrary.png",
+  },
+  {
+    id: 15,
+    text: "this candle looks interesting enough to take",
+    display: true,
+    options: [
+      {
+        text: "take the candle",
+        display: true,
+        nextText: 12,
+        setInventory: {
+          items: { redCandle: true },
+        },
+        hitFunction: ["SET_INVENTORY"],
+      },
+    ],
+    imgSrc: "/images/locations/pixelLibrary.png",
+  },
+  {
+    id: 16,
+    text: "you see one of the books looks out of place. Upon further investigation, this book is hollow! Inside, you find a mana potion and a large bronze key",
+    display: true,
+    options: [
+      {
+        text: "take the items",
+        display: true,
+        nextText: 12,
+        setInventory: {
+          items: { mana2: true, bronzeKey2: true },
+        },
+        hitFunction: ["SET_INVENTORY"],
+      },
+    ],
+    imgSrc: "/images/locations/pixelLibrary.png",
+  },
+  {
+    id: 17,
+    text: "On the table, there is a large black candle. Looking around, you see some drawers. All of them are locked except one.",
+    display: true,
+    options: [
+      {
+        text: "take the black candle",
+        display: true,
+        nextText: 12,
+        setInventory: {
+          items: { blackCandle: true },
+        },
+        hitFunction: ["SET_INVENTORY"],
+      },
+      {
+        text: "open the drawer",
+        display: true,
+        nextText: 18,
+      },
+    ],
+    imgSrc: "/images/locations/pixelLibrary.png",
+  },
+  {
+    id: 18,
+    text: "In the drawer, you find another piece of the map that looks like it lines up perfectly with the piece you already have.",
+    display: true,
+    options: [
+      {
+        text: "take the map piece",
+        display: true,
+        nextText: 12,
+        setInventory: {
+          items: { map2: true },
+        },
+        hitFunction: ["SET_INVENTORY"],
+      },
+    ],
+    imgSrc: "/images/locations/pixelLibrary.png",
+  },
+  {
+    // cathedral
+    id: 13,
+    text: "Cathedral... dark priest",
+    options: [
+      {
+        display: true,
+        text: "Place candles in respective holders and light",
+        setInventory: {
+          items: { redCandle: false, blackCandle: false },
+        },
+        hitFunction: ["SET_INVENTORY"],
+  
+        nextText: 19,
+      },
+    ],
+    imgSrc: "/images/locations/pixelCath.png",
+  },
+  {
+    id: 19,
+    text: "The dark ritual has begun...",
+    options: [
+      {
+        display: true,
+        text: "Exit into hallway",
+        nextText: 10,
+      },
+    ],
+    imgSrc: "/images/locations/pixelCath.png",
+  },
+  {
+    // must have keys in inventoy to open door... required inventory
+    id: 14,
+    text: "Open the door with keys and you see a large portrait of a woman. The painting seems to be moving... Intruder... handmaids",
+    options: [
+      {
+        display: true,
+        text: "Begin battle",
+        // encounterCode
+      },
+    ],
+    imgSrc: "/images/locations/pixelMother.png",
+  },
+    
+];
